7eab5639d12693049278aa18f8432f6ad42d8ef2
Xiang, Haihao <haihao.xiang@intel.com>
2012-01-30 15:17:30 +0800

    VPP: Reuse AVS kernel for pixel format conversion on Ivybridge
    Signed-off-by: Xiang, Haihao <haihao.xiang@intel.com>

--- src/i965_post_processing.c
+++ src/i965_post_processing.c
@@ -390,15 +390,19 @@ static const uint32_t pp_null_gen7[][4] = {
 };
 
 static const uint32_t pp_nv12_load_save_nv12_gen7[][4] = {
+#include "shaders/post_processing/gen7/pl2_to_pl2.g7b"
 };
 
 static const uint32_t pp_nv12_load_save_pl3_gen7[][4] = {
+#include "shaders/post_processing/gen7/pl2_to_pl3.g7b"
 };
 
 static const uint32_t pp_pl3_load_save_nv12_gen7[][4] = {
+#include "shaders/post_processing/gen7/pl3_to_pl2.g7b"
 };
 
 static const uint32_t pp_pl3_load_save_pl3_gen7[][4] = {
+#include "shaders/post_processing/gen7/pl3_to_pl3.g7b"
 };
 
 static const uint32_t pp_nv12_scaling_gen7[][4] = {
@@ -457,7 +461,7 @@ static struct pp_module pp_modules_gen7[] = {
             NULL,
         },
 
-        pp_plx_load_save_plx_initialize,
+        gen7_pp_plx_avs_initialize,
     },
 
     {
@@ -469,7 +473,7 @@ static struct pp_module pp_modules_gen7[] = {
             NULL,
         },
         
-        pp_plx_load_save_plx_initialize,
+        gen7_pp_plx_avs_initialize,
     },
 
     {
@@ -481,7 +485,7 @@ static struct pp_module pp_modules_gen7[] = {
             NULL,
         },
 
-        pp_plx_load_save_plx_initialize,
+        gen7_pp_plx_avs_initialize,
     },
 
     {
@@ -493,7 +497,7 @@ static struct pp_module pp_modules_gen7[] = {
             NULL,
         },
 
-        pp_plx_load_save_plx_initialize,
+        gen7_pp_plx_avs_initialize,
     },
 
     {
