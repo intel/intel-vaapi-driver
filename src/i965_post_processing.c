6a10eca3c6e06d1b71a104a7e782f97d9e0ba40f
Xiang, Haihao <haihao.xiang@intel.com>
2012-01-30 13:52:51 +0800

    VPP: Normal scaling on Ivybridge
    Need to adjust parameters later
    
    Signed-off-by: Xiang, Haihao <haihao.xiang@intel.com>

--- src/i965_post_processing.c
+++ src/i965_post_processing.c
@@ -402,6 +402,7 @@ static const uint32_t pp_pl3_load_save_pl3_gen7[][4] = {
 };
 
 static const uint32_t pp_nv12_scaling_gen7[][4] = {
+#include "shaders/post_processing/gen7/avs.g7b"
 };
 
 static const uint32_t pp_nv12_avs_gen7[][4] = {
@@ -504,7 +505,7 @@ static struct pp_module pp_modules_gen7[] = {
             NULL,
         },
 
-        pp_nv12_scaling_initialize,
+        gen7_pp_nv12_avs_initialize,
     },
 
     {
