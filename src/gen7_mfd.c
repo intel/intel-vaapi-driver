bcca7d23de29423b0d7d9954bfb21cc9bf3834f5
Gwenole Beauchesne <gwenole.beauchesne@intel.com>
2012-02-14 14:16:31 +0100

    Fix build with older VA-API (libva).
    Signed-off-by: Gwenole Beauchesne <gwenole.beauchesne@intel.com>

--- src/gen7_mfd.c
+++ src/gen7_mfd.c
@@ -1923,6 +1923,7 @@ gen7_mfd_vc1_decode_picture(VADriverContextP ctx,
     intel_batchbuffer_flush(batch);
 }
 
+#ifdef HAVE_JPEG_DECODING
 static void
 gen7_mfd_jpeg_decode_init(VADriverContextP ctx,
                           struct decode_state *decode_state,
@@ -2709,6 +2710,7 @@ gen7_mfd_jpeg_decode_picture(VADriverContextP ctx,
     intel_batchbuffer_end_atomic(batch);
     intel_batchbuffer_flush(batch);
 }
+#endif
 
 static void 
 gen7_mfd_decode_picture(VADriverContextP ctx, 
@@ -2740,9 +2742,11 @@ gen7_mfd_decode_picture(VADriverContextP ctx,
         gen7_mfd_vc1_decode_picture(ctx, decode_state, gen7_mfd_context);
         break;
 
+#ifdef HAVE_JPEG_DECODING
     case VAProfileJPEGBaseline:
         gen7_mfd_jpeg_decode_picture(ctx, decode_state, gen7_mfd_context);
         break;
+#endif
 
     default:
         assert(0);
