13a6dfe9379540721c11e119e1d785eeb1959160
Xiang, Haihao <haihao.xiang@intel.com>
2012-04-05 16:38:51 +0800

    Use the output from VME to format MFC_AVC_PAK_OBJEC command for Inter frame
    Signed-off-by: Xiang, Haihao <haihao.xiang@intel.com>

--- src/shaders/utils/mfc_batchbuffer_avc_inter.asm
+++ src/shaders/utils/mfc_batchbuffer_avc_inter.asm
@@ -24,24 +24,54 @@
  *    Xiang Haihao <haihao.xiang@intel.com>
  */
         
-__PAK_OBJECT:   
+__PAK_OBJECT:
+        mul             (1)     tmp_offset.0<1>:ud              width_in_mb<0,1,0>:uw           mb_y<0,1,0>:ub {align1};
+        add             (1)     tmp_offset.0<1>:ud              tmp_offset.0<0,1,0>:ud          mb_x<0,1,0>:ub {align1};
+        /*
+         * The layout of VME output
+         * ++++++++++++++++++++++++++++++++++++++++++++++
+         * |    MV(128bytes)  |   other info (32bytes)  |
+         * ++++++++++++++++++++++++++++++++++++++++++++++
+         */
+        mul             (1)     tmp_vme_output.8<1>:ud          tmp_offset.0<0,1,0>:ud          INTER_VME_OUTPUT_IN_OWS:ud {align1} ;   /* point to output buffer */
+        add             (1)     tmp_vme_output.8<1>:ud          tmp_vme_output.8<0,1,0>:ud      INTER_VME_OUTPUT_MV_IN_OWS:uw {align1}; /* point to other info */
+        
         mov             (16)    pak_object_ud<1>:ud             0x0:ud {align1} ;
         /* DW0 */
         mov             (1)     pak_object0_ud<1>:ud            MFC_AVC_PAK_OBJECT_INTER_DW0 ;
         /* DW1 */
         mov             (1)     pak_object1_ud<1>:ud            MFC_AVC_PAK_OBJECT_INTER_DW1 ;
         /* DW2 */
-        mul             (1)     pak_object2_ud<1>:ud            width_in_mb<0,1,0>:uw           mb_y<0,1,0>:ub {align1};
-        add             (1)     pak_object2_ud<1>:ud            pak_object2_ud<0,1,0>:ud        mb_x<0,1,0>:ub {align1};
-        mul             (1)     pak_object2_ud<1>:ud            pak_object2_ud<0,1,0>:ud        INTER_VME_OUTPUT_IN_BYTES:ud {align1} ;
-        /* DW3 */
-        mov             (1)     pak_object3_ud<1>:ud            MFC_AVC_PAK_OBJECT_INTER_DW3 ;
+        mul             (1)     pak_object2_ud<1>:ud            tmp_offset.0<0,1,0>:ud          INTER_VME_OUTPUT_IN_BYTES:ud {align1} ;
+        
         /* DW5 */
         mov             (1)     pak_object5_ud<1>:ud            MFC_AVC_PAK_OBJECT_INTRA_DW5 ;
         
         and.z.f0.1      (1)     null<1>:uw                      flags<0,1,0>:uw         FLAG_MASK_LAST_OBJECT {align1};
         
 __PAK_OBJECT_LOOP:
+        mov             (8)     msg_reg0.0<1>:ud                tmp_vme_output<8,8,1>:ud {align1} ;
+        
+send (16)
+        msg_ind
+        ob_read_wb
+        null
+        data_port(
+                OB_CACHE_TYPE,
+                OB_READ,
+                OB_CONTROL_2,
+                BIND_IDX_VME_OUTPUT,
+                OB_WRITE_COMMIT_CATEGORY,
+                OB_HEADER_PRESENT
+        )
+        mlen 1
+        rlen ob_read_wb_len_vme_inter
+        {align1};
+
+        /* DW3 */
+        and             (1)     pak_object3_ud<1>:ud            ob_read_wb0.0<0,1,0>:ud         0xFFFF {align1} ;
+        add             (1)     pak_object3_ud<1>:ud            pak_object3_ud<0,1,0>:ud        MFC_AVC_PAK_OBJECT_INTER_DW3 {align1} ;
+        
         /* DW4 */
         add             (1)     pak_object4_ud<1>:ud            mb_xy<0,1,0>:uw                 MFC_AVC_PAK_OBJECT_INTER_DW4 {align1} ;
         add             (1)     mb_x<1>:ub                      mb_x<0,1,0>:ub                  1:uw {align1};
@@ -56,9 +86,15 @@ __PAK_OBJECT_LOOP:
         (-f0.0)mov      (1)     pak_object6_ud<1>:ud            0x0:ud {align1} ;
         add             (1)     pak_object6_ud<1>:ud            pak_object6_ud<0,1,0>:ud        qp<0,1,0>:ub {align1} ;
 
+        /* DW7 */
+        mov             (1)     pak_object7_ud<1>:ud            ob_read_wb0.4<0,1,0>:ud {align1} ;
+        
         mov             (8)     msg_reg0.0<1>:ud                tmp_mfc_batchbuffer<8,8,1>:ud {align1} ;
         mov             (8)     msg_reg1.0<1>:ud                pak_object_ud<8,8,1>:ud {align1} ;
         mov             (8)     msg_reg2.0<1>:ud                pak_object8_ud<8,8,1>:ud {align1} ;
+
+        /* point to the next other info block */
+        add             (1)     tmp_vme_output.8<1>:ud          tmp_vme_output.8<0,1,0>:ud      INTER_VME_OUTPUT_IN_OWS:ud {align1} ;
         
 send (16)
         msg_ind
