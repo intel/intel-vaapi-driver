f106ee6aecb5654668d95cae9fc8579029a2d08d
Xiang, Haihao <haihao.xiang@intel.com>
2012-06-28 11:35:50 +0800

    VPP: New shaders for color conversion between packed and planar YUV on IVB
    Signed-off-by: Xiang, Haihao <haihao.xiang@intel.com>

---
+++ src/shaders/post_processing/gen7/pa_to_pl3.asm
@@ -0,0 +1,17 @@
+// Module name: AVS
+.kernel PA_TO_PL3
+.code
+
+#include "VP_Setup.g4a"
+#include "Set_Layer_0.g4a"
+#include "Set_AVS_Buf_0123_VYUA.g4a"
+#include "PA_AVS_Buf_0.g4a"
+#include "PA_AVS_Buf_1.g4a"
+#include "PA_AVS_Buf_2.g4a"
+#include "PA_AVS_Buf_3.g4a"
+#include "Save_AVS_PL3.g4a"        
+#include "EOT.g4a"
+
+.end_code  
+
+.end_kernel
