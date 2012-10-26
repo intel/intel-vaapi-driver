0d1dfb3988f530795952df09b4121c775896887b
Li, Xiaowei A <xiaowei.a.li@intel.com>
2012-10-26 09:34:38 +0800

    VPP: Add shaders for video process on Ivybridge.
    Signed-off-by: Xiang, Haihao <haihao.xiang@intel.com>

---
+++ src/shaders/post_processing/gen7/avs.asm
@@ -0,0 +1,19 @@
+// Module name: AVS
+.kernel AVS
+.code
+
+#include "VP_Setup.g4a"
+#include "Set_Layer_0.g4a"
+#include "Set_AVS_Buf_0123_PL2.g4a"
+#include "PL2_AVS_Buf_0.g4a"
+#include "PL2_AVS_Buf_1.g4a"
+#include "PL2_AVS_Buf_2.g4a"
+#include "PL2_AVS_Buf_3.g4a"
+#include "Save_AVS_NV12.g4a"        
+#include "EOT.g4a"
+
+.end_code  
+
+.end_kernel
+
+// end of DNDI
