75f5fccd9c57e70955162e1c91fd649e4b93ea38
Zhao Yakui <yakui.zhao@intel.com>
2013-05-23 10:22:09 +0800

    Add the VPP shader of YV12/I420->NV12 conversion
    Signed-off-by: Zhao Yakui <yakui.zhao@intel.com>

--- src/i965_post_processing.c
+++ src/i965_post_processing.c
@@ -1176,7 +1176,7 @@ static const uint32_t pp_nv12_load_save_pl3_gen8[][4] = {
 };
 
 static const uint32_t pp_pl3_load_save_nv12_gen8[][4] = {
-#include "shaders/post_processing/gen7/pl3_to_pl2.g75b"
+#include "shaders/post_processing/gen8/pl3_to_pl2.g8b"
 };
 
 static const uint32_t pp_pl3_load_save_pl3_gen8[][4] = {
