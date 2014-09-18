cb8d009273265e165e6f2ed6f1906c03726a267f
Emil Velikov <emil.l.velikov@gmail.com>
2014-09-18 17:45:39 +0100

    autogen.sh: fix out of tree invocation
    Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,14 @@
 #! /bin/sh
 
-autoreconf -v --install
+srcdir=`dirname "$0"`
+test -z "$srcdir" && srcdir=.
+
+ORIGDIR=`pwd`
+cd "$srcdir"
+
+autoreconf -v --install || exit 1
+cd $ORIGDIR || exit $?
 
 if test -z "$NOCONFIGURE"; then
-    ./configure "$@"
+    "$srcdir"/configure "$@"
 fi
