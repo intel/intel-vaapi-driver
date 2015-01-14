592f4fe2d5220e41ad5431539d56d643d2245beb
Xiang, Haihao <haihao.xiang@intel.com>
2015-01-14 11:59:48 +0800

    Update license header for some shaders
    Signed-off-by: Xiang, Haihao <haihao.xiang@intel.com>
    (cherry picked from commit 7e444da6f69a642267fbd16257d0e8615d36173c)

--- src/shaders/vme/inter_frame.asm
+++ src/shaders/vme/inter_frame.asm
@@ -1,9 +1,25 @@
 /*
  * Copyright © <2010>, Intel Corporation.
  *
- * This program is licensed under the terms and conditions of the
- * Eclipse Public License (EPL), version 1.0.  The full text of the EPL is at
- * http://www.opensource.org/licenses/eclipse-1.0.php.
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sub license, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the
+ * next paragraph) shall be included in all copies or substantial portions
+ * of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT.
+ * IN NO EVENT SHALL PRECISION INSIGHT AND/OR ITS SUPPLIERS BE LIABLE FOR
+ * ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
  */
 // Modual name: IntraFrame.asm
