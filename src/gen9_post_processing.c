f365892c6710be2d4e29759b99afb57903f62b59
Sreerenj Balachandran <sreerenj.balachandran@intel.com>
2015-10-12 11:37:57 +0300

    vpp/gen9: Fix the incorrect loading of kernel module.
    The correct i965_kernel interace index for pp_pl3_load_save_pl3_gen9 is PP_PL3_LOAD_SAVE_PL3.
    
    Signed-off-by: Sreerenj Balachandran <sreerenj.balachandran@intel.com>

--- src/gen9_post_processing.c
+++ src/gen9_post_processing.c
@@ -154,7 +154,7 @@ static struct pp_module pp_modules_gen9[] = {
     {
         {
             "PL3_PL3",
-            PP_PL3_LOAD_SAVE_N12,
+            PP_PL3_LOAD_SAVE_PL3,
             pp_pl3_load_save_pl3_gen9,
             sizeof(pp_pl3_load_save_pl3_gen9),
             NULL,
