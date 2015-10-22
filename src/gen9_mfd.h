823d9bbe2baf915b7d8ee0fc86fb774126a9148c
Pengfei Qu <Pengfei.Qu@intel.com>
2015-10-22 04:51:38 -0400

    VP9 HWDec: add VP9 HW decoding pipeline
    v2: fix sengment feature in inter frame
    
    v3: update the prob buffer logic
    
    v4: fix intra-only feature issue
    
    Signed-off-by: Pengfei Qu <Pengfei.Qu@intel.com>
    Signed-off-by: Sirisha Muppavarapu <sirisha.muppavarapu@intel.com>
    Signed-off-by: peng.chen <peng.c.chen@intel.com>
    Reviewed-by: Zhao Yakui <yakui.zhao@intel.com>
    (cherry picked from commit 8f1e1015a4cc8c08dd685f80829ca1555ae1cab1)

--- src/gen9_mfd.h
+++ src/gen9_mfd.h
@@ -34,8 +34,23 @@
 #include <i915_drm.h>
 #include <intel_bufmgr.h>
 #include "i965_decoder.h"
+#include "vp9_probs.h"
 
 struct hw_context;
+
+typedef struct vp9_frame_status
+{
+    uint16_t frame_width;
+    uint16_t frame_height;
+    uint8_t frame_type;
+    uint8_t show_frame;
+    uint8_t refresh_frame_context;
+    uint8_t frame_context_idx;
+    uint8_t intra_only;
+    uint8_t prob_buffer_saved_flag;
+    uint8_t prob_buffer_restored_flag;
+    dri_bo * mv_temporal_buffer_bo;
+}vp9_last_frame_status;
 
 struct gen9_hcpd_context
 {
@@ -65,10 +80,21 @@ struct gen9_hcpd_context
     GenBuffer sao_line_buffer;
     GenBuffer sao_tile_line_buffer;
     GenBuffer sao_tile_column_buffer;
+    GenBuffer hvd_line_rowstore_buffer;
+    GenBuffer hvd_tile_rowstore_buffer;
+    GenBuffer vp9_probability_buffer;
+    GenBuffer vp9_segment_id_buffer;
 
     unsigned short first_inter_slice_collocated_ref_idx;
     unsigned short first_inter_slice_collocated_from_l0_flag;
     int first_inter_slice_valid;
+
+    vp9_last_frame_status last_frame;
+    FRAME_CONTEXT vp9_frame_ctx[FRAME_CONTEXTS];
+    FRAME_CONTEXT vp9_fc_inter_default;
+    FRAME_CONTEXT vp9_fc_key_default;
+    FRAME_CONTEXT vp9_fc;
+    FRAME_CONTEXT vp9_saved_fc;
 };
 
 #endif /* GEN9_MFD_H */
