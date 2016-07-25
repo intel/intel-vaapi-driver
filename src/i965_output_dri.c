396bf01a6181401e14d8d60f34774109239c8fd1
Scott D Phillips <scott.d.phillips@intel.com>
2016-07-25 13:19:28 -0700

    dri: return error for unimplemented surface formats
    Previously packed YUV422 surface were allowed to be renderd but
    got rendered improperly.
    
    Signed-off-by: Scott D Phillips <scott.d.phillips@intel.com>
    (cherry picked from commit c7c69eb953822bbaf2075f9bea6f52d41212f5d3)

--- src/i965_output_dri.c
+++ src/i965_output_dri.c
@@ -135,6 +135,9 @@ i965_put_surface_dri(
      */
     obj_surface = SURFACE(surface);
     ASSERT_RET(obj_surface && obj_surface->bo, VA_STATUS_SUCCESS);
+    ASSERT_RET(obj_surface->fourcc != VA_FOURCC_YUY2 &&
+               obj_surface->fourcc != VA_FOURCC_UYVY,
+               VA_STATUS_ERROR_UNIMPLEMENTED);
 
     _i965LockMutex(&i965->render_mutex);
 
