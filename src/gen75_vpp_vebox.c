intel/intel-vaapi-driver

commit 51ad826fcc0d2512f7ef74e807e4b8526663fc28

Fix to use source and output regions size instead of the input output surfaces original size.
This fixes https://bugs.freedesktop.org/show_bug.cgi?id=96739

Signed-off-by: Ung, Teng En <teng.en.ung@intel.com>
Reviewed-by: Zhao Yakui <yakui.zhao@intel.com>

src/gen75_vpp_vebox.c
@@ -1379,11 +1379,11 @@ int hsw_veb_pre_format_convert(VADriverContextP ctx,
 
     proc_ctx->format_convert_flags = 0;
 
-    proc_ctx->width_input   = obj_surf_input->orig_width;
-    proc_ctx->height_input  = obj_surf_input->orig_height;
-    proc_ctx->width_output  = obj_surf_output->orig_width;
-    proc_ctx->height_output = obj_surf_output->orig_height;
-   
+    proc_ctx->width_input   = proc_ctx->pipeline_param->surface_region->width;
+    proc_ctx->height_input  = proc_ctx->pipeline_param->surface_region->height;
+    proc_ctx->width_output  = proc_ctx->pipeline_param->output_region->width;
+    proc_ctx->height_output = proc_ctx->pipeline_param->output_region->height;
+
     /* only partial frame is not supported to be processed */
     /*
     assert(proc_ctx->width_input   == proc_ctx->pipeline_param->surface_region->width);

commit 4f8d4b211b4f90ef26c356b8028c5435cd685952

VPP: Check the VPP pipeline_parameter to avoid NULL pointer
This is to fix the crash issue caused by the commit
51ad826fcc0d2512f7ef74e807e4b8526663fc28.

Reported-by: Xu,Guangxin <guangxin.xu@itnel.com>
Signed-off-by: Zhao Yakui <yakui.zhao@intel.com>

src/gen75_vpp_vebox.c
@@ -1379,10 +1379,29 @@ int hsw_veb_pre_format_convert(VADriverContextP ctx,
 
     proc_ctx->format_convert_flags = 0;
 
-    proc_ctx->width_input   = proc_ctx->pipeline_param->surface_region->width;
-    proc_ctx->height_input  = proc_ctx->pipeline_param->surface_region->height;
-    proc_ctx->width_output  = proc_ctx->pipeline_param->output_region->width;
-    proc_ctx->height_output = proc_ctx->pipeline_param->output_region->height;
+    if ((obj_surf_input == NULL) &&
+        (proc_ctx->pipeline_param->surface_region == NULL))
+        assert(0);
+
+    if ((obj_surf_output == NULL) &&
+        (proc_ctx->pipeline_param->output_region == NULL))
+        assert(0);
+
+    if (proc_ctx->pipeline_param->surface_region) {
+        proc_ctx->width_input   = proc_ctx->pipeline_param->surface_region->width;
+        proc_ctx->height_input  = proc_ctx->pipeline_param->surface_region->height;
+    } else {
+        proc_ctx->width_input   = obj_surf_input->orig_width;
+        proc_ctx->height_input  = obj_surf_input->orig_height;
+    }
+
+    if (proc_ctx->pipeline_param->output_region) {
+        proc_ctx->width_output  = proc_ctx->pipeline_param->output_region->width;
+        proc_ctx->height_output = proc_ctx->pipeline_param->output_region->height;
+    } else {
+        proc_ctx->width_output  = obj_surf_output->orig_width;
+        proc_ctx->height_output = obj_surf_output->orig_height;
+    }
 
     /* only partial frame is not supported to be processed */
     /*