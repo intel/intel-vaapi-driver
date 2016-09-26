78f5a27d03cbfc47323f982d2c11c6ec46ad914e
U. Artie Eoff <ullysses.a.eoff@intel.com>
2016-09-26 13:11:47 -0700

    test: add common test utils
    Common utilities and functions that may be useful for multiple
    tests.
    
    Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>
    Reviewed-by: Zhao Yakui <yakui.zhao@intel.com>
    (cherry picked from commit 1c1e91cb597910111c5acea238d68d666c515655)

---
+++ test/test_utils.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2016 Intel Corporation. All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sub license, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the
+ * next paragraph) shall be included in all copies or substantial portions
+ * of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT.
+ * IN NO EVENT SHALL PRECISION INSIGHT AND/OR ITS SUPPLIERS BE LIABLE FOR
+ * ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#ifndef TEST_UTILS_H
+#define TEST_UTILS_H
+
+#include <random>
+
+template <typename T>
+class RandomValueGenerator
+{
+public:
+    RandomValueGenerator(const T& min, const T& max)
+        : dis(min, max)
+    { }
+
+    const T operator()()
+    {
+        static std::random_device rd;
+        static std::mt19937 gen(rd());
+        return dis(gen);
+    }
+
+private:
+    std::uniform_int_distribution<T> dis;
+};
+
+#endif // TEST_UTILS_H
