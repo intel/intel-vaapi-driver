2e86d9a738563d66945cc4dd61768d00315c747c
Xiang, Haihao <haihao.xiang@intel.com>
2016-10-13 03:07:45 +0800

    Use Media Read message if possible on Gen8+
    AVS can't gurantee bit-match for a large surface. This fixes the
    failure reported by gtest case Common/JPEGEncodeInputTest.Full/95.
    
    before:
    [  FAILED  ] Common/JPEGEncodeInputTest.Full/95, where GetParam() = (Fixed Size 7680x4320, 0x501176 pointing to "I420") (9239 ms)
    [----------] 1 test from Common/JPEGEncodeInputTest (9239 ms total)
    
    [----------] Global test environment tear-down
    [==========] 1 test from 1 test case ran. (9361 ms total)
    [  PASSED  ] 0 tests.
    [  FAILED  ] 1 test, listed below:
    [  FAILED  ] Common/JPEGEncodeInputTest.Full/95, where GetParam() = (Fixed Size 7680x4320, 0x501176 pointing to "I420")
    
    after:
    [       OK ] Common/JPEGEncodeInputTest.Full/95 (15250 ms)
    [----------] 1 test from Common/JPEGEncodeInputTest (15250 ms total)
    
    [----------] Global test environment tear-down
    [==========] 1 test from 1 test case ran. (15365 ms total)
    [  PASSED  ] 1 test.
    
    Signed-off-by: Xiang, Haihao <haihao.xiang@intel.com>
    Reviewed-by: Zhao Yakui <yakui.zhao@intel.com>
    Tested-by: Sean V Kelley <seanvk@posteo.de>
    (cherry picked from commit 78b6841fa3e247f15005ea3450d8404c59938ac3)

--- src/shaders/post_processing/gen8/pl3_to_pl2.asm
+++ src/shaders/post_processing/gen8/pl3_to_pl2.asm
@@ -5,10 +5,12 @@
 #include "VP_Setup.g8a"
 #include "Set_Layer_0.g8a"
 #include "Set_AVS_Buf_0123_PL3.g8a"
+#include "PL3_media_read_buf0123.g8a"
 #include "PL3_AVS_Buf_0.g8a"
 #include "PL3_AVS_Buf_1.g8a"
 #include "PL3_AVS_Buf_2.g8a"
 #include "PL3_AVS_Buf_3.g8a"
+__SAVE_BUF0123:
 #include "Save_AVS_NV12.g8a"        
 #include "EOT.g8a"
 
