f661761e20bf73e0f14b1b2a5a3622b4aaba884f
Xiang, Haihao <haihao.xiang@intel.com>
2017-03-24 09:13:24 +0800

    Add pre-commit hook
    Check that the code follows a consistant coding style before committing.
    The current command will be interrupted if the commit doesn't follow the
    code style
    
    This fixes https://github.com/01org/intel-vaapi-driver/issues/99
    
    Signed-off-by: Xiang, Haihao <haihao.xiang@intel.com>
    (cherry picked from commit 2f0a8449f37b978eb202082b9113039b00c6931a)

--- autogen.sh
+++ autogen.sh
@@ -6,6 +6,15 @@ test -z "$srcdir" && srcdir=.
 ORIGDIR=`pwd`
 cd "$srcdir"
 
+# install pre-commit hook
+SRC_PRE_COMMIT=hooks/pre-commit.hook
+GIT_PRE_COMMIT=.git/hooks/pre-commit
+
+if [ ! \( -x $GIT_PRE_COMMIT -a -L $GIT_PRE_COMMIT \) ]; then
+    rm -f $GIT_PRE_COMMIT
+    ln -s ../../$SRC_PRE_COMMIT $GIT_PRE_COMMIT
+fi
+
 autoreconf -v --install || exit 1
 cd $ORIGDIR || exit $?
 
