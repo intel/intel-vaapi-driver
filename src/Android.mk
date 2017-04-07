2a6b48d928ec36698a5200369f8d4cd0692fbe8e
Chih-Wei Huang <cwhuang@linux.org.tw>
2017-04-07 11:01:01 +0800

    Move source lists into Makefile.sources
    Factor the source lists into a shared makefile. This prevents duplicate of
    source lists, and hence prevents the Android build from breaking as often.
    
    Signed-off-by: Chih-Wei Huang <cwhuang@linux.org.tw>

--- src/Android.mk
+++ src/Android.mk
@@ -24,56 +24,11 @@
 
 LOCAL_PATH:= $(call my-dir)
 
+include $(LOCAL_PATH)/Makefile.sources
+
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES :=              \
-	dso_utils.c		\
-	gen6_mfc.c		\
-	gen6_mfc_common.c	\
-	gen6_mfd.c		\
-	gen6_vme.c		\
-	gen7_vme.c		\
-	gen7_mfc.c		\
-	gen7_mfd.c		\
-	gen75_mfd.c		\
-	gen75_mfc.c		\
-	gen8_mfc.c		\
-	gen8_mfd.c		\
-	gen8_vme.c		\
-	gen9_vme.c		\
-	gen9_mfc.c		\
-	gen9_mfc_hevc.c		\
-	gen9_mfd.c		\
-	gen75_picture_process.c	\
-	gen75_vme.c		\
-	gen75_vpp_gpe.c  	\
-	gen75_vpp_vebox.c	\
-	gen9_post_processing.c	\
-	i965_avc_bsd.c		\
-	i965_avc_hw_scoreboard.c\
-	i965_avc_ildb.c		\
-	i965_decoder_utils.c	\
-	i965_device_info.c	\
-	i965_drv_video.c	\
-	i965_encoder.c		\
-	i965_encoder_utils.c	\
-	i965_media.c		\
-	i965_media_h264.c	\
-	i965_media_mpeg2.c	\
-	i965_gpe_utils.c	\
-	i965_post_processing.c	\
-	gen8_post_processing.c	\
-	i965_render.c		\
-	i965_vpp_avs.c		\
-	gen8_render.c		\
-	gen9_render.c		\
-	intel_batchbuffer.c	\
-	intel_batchbuffer_dump.c\
-	intel_driver.c		\
-	intel_memman.c		\
-	object_heap.c		\
-	intel_media_common.c		\
-	$(NULL)
+LOCAL_SRC_FILES := $(source_c)
 
 LOCAL_MODULE := i965_drv_video
 LOCAL_MODULE_TAGS := optional
