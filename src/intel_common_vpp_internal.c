3ad971703c8abcad90013c1a53fb6cc6fc9e1145
Xiang, Haihao <haihao.xiang@intel.com>
2017-08-07 16:23:21 +0800

    Add a new shader for CSC/scaling from P010/I010 to YUY2/UYVY/NV12/I420 on GEN9
    With the new shader, the driver can convert 10bit surface to 8bit
    surface in a single step
    
    This fixes https://github.com/01org/intel-vaapi-driver/issues/212
    
    Signed-off-by: Xiang, Haihao <haihao.xiang@intel.com>

--- src/intel_common_vpp_internal.c
+++ src/intel_common_vpp_internal.c
@@ -65,6 +65,26 @@ intel_yuv420p8_scaling_post_processing(
     return va_status;
 }
 
+static VAStatus
+intel_10bit_8bit_scaling_post_processing(VADriverContextP   ctx,
+                                         struct i965_post_processing_context *pp_context,
+                                         struct i965_surface *src_surface,
+                                         VARectangle *src_rect,
+                                         struct i965_surface *dst_surface,
+                                         VARectangle *dst_rect)
+{
+    struct i965_driver_data *i965 = i965_driver_data(ctx);
+    VAStatus va_status = VA_STATUS_ERROR_UNIMPLEMENTED;
+
+    if (IS_GEN9(i965->intel.device_info))
+        va_status = gen9_10bit_8bit_scaling_post_processing(ctx, pp_context,
+                                                            src_surface,
+                                                            src_rect,
+                                                            dst_surface,
+                                                            dst_rect);
+
+    return va_status;
+}
 
 VAStatus
 intel_common_scaling_post_processing(VADriverContextP ctx,
@@ -85,18 +105,22 @@ intel_common_scaling_post_processing(VADriverContextP ctx,
     /* The Bit 2 is used to indicate that it is 10bit or 8bit.
      * The Bit 0/1 is used to indicate the 420/422/444 format
      */
+#define SRC_8BIT_420     (1 << 0)
+#define SRC_8BIT_422     (2 << 0)
+#define SRC_8BIT_444     (3 << 0)
 #define SRC_10BIT_420    (5 << 0)
 #define SRC_10BIT_422    (6 << 0)
 #define SRC_10BIT_444    (7 << 0)
-#define SRC_8BIT_420     (1 << 0)
 
     /* The Bit 6 is used to indicate that it is 10bit or 8bit.
      * The Bit 5/4 is used to indicate the 420/422/444 format
      */
+#define DST_8BIT_420     (1 << 4)
+#define DST_8BIT_422     (2 << 0)
+#define DST_8BIT_444     (3 << 0)
 #define DST_10BIT_420    (5 << 4)
 #define DST_10BIT_422    (6 << 4)
 #define DST_10BIT_444    (7 << 4)
-#define DST_8BIT_420     (1 << 4)
 
 #define SRC_YUV_PACKED   (1 << 3)
 #define DST_YUV_PACKED   (1 << 7)
@@ -104,6 +128,8 @@ intel_common_scaling_post_processing(VADriverContextP ctx,
 #define MASK_CSC         (0xFF)
 #define SCALE_10BIT_10BIT_420   (SRC_10BIT_420 | DST_10BIT_420)
 #define SCALE_8BIT_8BIT_420     (SRC_8BIT_420 | DST_8BIT_420)
+#define SCALE_10BIT420_8BIT422  (SRC_10BIT_420 | DST_8BIT_422 | DST_YUV_PACKED)
+#define SCALE_10BIT420_8BIT420  (SRC_10BIT_420 | DST_8BIT_420)
 
     if (src_fourcc == VA_FOURCC_P010 ||
         src_fourcc == VA_FOURCC_I010)
@@ -113,6 +139,10 @@ intel_common_scaling_post_processing(VADriverContextP ctx,
         src_fourcc == VA_FOURCC_I420)
         scale_flag |= SRC_8BIT_420;
 
+    if (src_fourcc == VA_FOURCC_YUY2 ||
+        src_fourcc == VA_FOURCC_UYVY)
+        scale_flag |= (SRC_8BIT_422 | SRC_YUV_PACKED);
+
     if (dst_fourcc == VA_FOURCC_P010 ||
         dst_fourcc == VA_FOURCC_I010)
         scale_flag |= DST_10BIT_420;
@@ -121,12 +151,20 @@ intel_common_scaling_post_processing(VADriverContextP ctx,
         dst_fourcc == VA_FOURCC_I420)
         scale_flag |= DST_8BIT_420;
 
+    if (dst_fourcc == VA_FOURCC_YUY2 ||
+        dst_fourcc == VA_FOURCC_UYVY)
+        scale_flag |= (DST_8BIT_422 | DST_YUV_PACKED);
+
+    if (dst_fourcc == VA_FOURCC_YUY2 ||
+        dst_fourcc == VA_FOURCC_UYVY)
+        scale_flag |= (DST_8BIT_422 | DST_YUV_PACKED);
+
     /* If P010 is converted without resolution change,
      * fall back to VEBOX
      */
     if (i965->intel.has_vebox &&
         (src_fourcc == VA_FOURCC_P010) &&
-        (dst_fourcc == VA_FOURCC_P010) &&
+        (dst_fourcc == VA_FOURCC_P010 || dst_fourcc == VA_FOURCC_NV12) &&
         (src_rect->width == dst_rect->width) &&
         (src_rect->height == dst_rect->height))
         scale_flag = 0;
@@ -160,6 +198,22 @@ intel_common_scaling_post_processing(VADriverContextP ctx,
         status = intel_yuv420p8_scaling_post_processing(ctx, pp_context,
                                                         (struct i965_surface *)src_surface, (VARectangle *)src_rect,
                                                         dst_surface, &aligned_dst_rect);
+    }
+
+    if (((scale_flag & MASK_CSC) == SCALE_10BIT420_8BIT420 ||
+         (scale_flag & MASK_CSC) == SCALE_10BIT420_8BIT422) &&
+        (pp_context->scaling_gpe_context_initialized & VPPGPE_10BIT_8BIT)) {
+
+        tmp_x = ALIGN_FLOOR(dst_rect->x, 4);
+        tmp_width = dst_rect->x + dst_rect->width - tmp_x;
+        aligned_dst_rect.x = tmp_x;
+        aligned_dst_rect.width = tmp_width;
+        aligned_dst_rect.y = dst_rect->y;
+        aligned_dst_rect.height = dst_rect->height;
+
+        status = intel_10bit_8bit_scaling_post_processing(ctx, pp_context,
+                                                          (struct i965_surface *)src_surface, (VARectangle *)src_rect,
+                                                          dst_surface, &aligned_dst_rect);
     }
 
     return status;
