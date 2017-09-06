df6d38726cfb3870c17dfedcdc052d0986ab2c03
Wang Tiatian <tiantian.wang@intel.com>
2017-09-06 11:01:42 -0400

    add gen8 avc encoder kernel
    Signed-off-by: Wang Tiatian <tiantian.wang@intel.com>
    
    Conflicts:
    	src/gen9_avc_encoder_kernels.c

--- src/gen9_avc_encoder_kernels.h
+++ src/gen9_avc_encoder_kernels.h
@@ -39,4 +39,6 @@ extern const unsigned int skl_avc_fei_encoder_kernels[AVC_ENC_FEI_SKL_SZ];
 #define AVC_ENC_KBL_SZ 101994
 extern const unsigned int kbl_avc_encoder_kernels[AVC_ENC_KBL_SZ];
 
+#define AVC_ENC_BDW_SZ  95532
+extern const unsigned int bdw_avc_encoder_kernels[AVC_ENC_BDW_SZ];
 #endif//_GEN9_AVC_ENCODER_KERNELS_H
