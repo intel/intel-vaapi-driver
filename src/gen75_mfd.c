4c2eb9d2d3f639c7f3f13e97e7035205393c34bc
carpalis <jerome.borsboom@carpalis.nl>
2017-10-22 16:30:47 +0200

    genX_mfd: do not scale dmv buffer with height
    PRM states that for VC-1 the direct motion vector buffer may be scaled
    with frame height but should not be scaled with frame width. Hardware
    assumes frame width is fixed at 128 MBs.
    
    Signed-off-by: Jerome Borsboom <jerome.borsboom@carpalis.nl>

--- src/gen75_mfd.c
+++ src/gen75_mfd.c
@@ -1505,7 +1505,6 @@ gen75_mfd_init_vc1_surface(VADriverContextP ctx,
 {
     struct i965_driver_data *i965 = i965_driver_data(ctx);
     struct gen7_vc1_surface *gen7_vc1_surface = obj_surface->private_data;
-    int width_in_mbs = ALIGN(pic_param->coded_width, 16) / 16;
     int height_in_mbs = ALIGN(pic_param->coded_height, 16) / 16;
 
     obj_surface->free_private_data = gen75_mfd_free_vc1_surface;
@@ -1525,7 +1524,7 @@ gen75_mfd_init_vc1_surface(VADriverContextP ctx,
     if (gen7_vc1_surface->dmv == NULL) {
         gen7_vc1_surface->dmv = dri_bo_alloc(i965->intel.bufmgr,
                                              "direct mv w/r buffer",
-                                             width_in_mbs * height_in_mbs * 64,
+                                             128 * height_in_mbs * 64,
                                              0x1000);
     }
 }
