06e9589f88090cb9186b3435a1b2665713f92d6f
carpalis <jerome.borsboom@carpalis.nl>
2017-10-24 20:30:16 +0200

    genX_mfd: fix BitplanePresentFlag for skipped pictures
    Skipped pictures do not pass a bitplane buffer. We do, however,
    locally encode a bitplane buffer with only the SKIPMB bit set. The
    presence of this buffer must be passed through the BitplanePresentFlag
    hardware field.
    
    Signed-off-by: Jerome Borsboom <jerome.borsboom@carpalis.nl>

--- src/gen8_mfd.c
+++ src/gen8_mfd.c
@@ -1480,6 +1480,7 @@ gen8_mfd_vc1_pic_state(VADriverContextP ctx,
     int overlap = 0;
     int interpolation_mode = 0;
     int loopfilter = 0;
+    int bitplane_present;
 
     assert(decode_state->pic_param && decode_state->pic_param->buffer);
     pic_param = (VAPictureParameterBufferVC1 *)decode_state->pic_param->buffer;
@@ -1563,10 +1564,12 @@ gen8_mfd_vc1_pic_state(VADriverContextP ctx,
     if (pic_param->b_picture_fraction < 21)
         scale_factor = b_picture_scale_factor[pic_param->b_picture_fraction];
 
-    if (picture_type == GEN7_VC1_SKIPPED_PICTURE)
+    if (picture_type == GEN7_VC1_SKIPPED_PICTURE) {
         ptype = GEN7_VC1_P_PICTURE;
-    else {
+        bitplane_present = 1;
+    } else {
         ptype = pic_param->picture_fields.bits.picture_type;
+        bitplane_present = !!(pic_param->bitplane_present.value & 0x7f);
         loopfilter = pic_param->entrypoint_fields.bits.loopfilter;
     }
 
@@ -1697,7 +1700,7 @@ gen8_mfd_vc1_pic_state(VADriverContextP ctx,
                   pic_param->pic_quantizer_fields.bits.half_qp << 1 |
                   pic_param->pic_quantizer_fields.bits.pic_quantizer_type << 0);
     OUT_BCS_BATCH(batch,
-                  !!(pic_param->bitplane_present.value & 0x7f) << 31 |
+                  bitplane_present << 31 |
                   pic_param->raw_coding.flags.forward_mb << 30 |
                   pic_param->raw_coding.flags.mv_type_mb << 29 |
                   pic_param->raw_coding.flags.skip_mb << 28 |
